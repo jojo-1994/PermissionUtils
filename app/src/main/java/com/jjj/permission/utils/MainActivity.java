dcde302ccaf04c203612de5bd55a2f07e472e26d
姜姣姣 <jiangjiaojiao@163.com>
2020-04-13 12:25:10 +0800

    Add demo code

--- app/src/main/java/com/jjj/permission/utils/MainActivity.java
+++ app/src/main/java/com/jjj/permission/utils/MainActivity.java
@@ -1,16 +1,42 @@
 package com.jjj.permission.utils;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
 import android.os.Bundle;
 
+import com.jjj.permission.PermissionCallback;
+import com.jjj.permission.PermissionUtils;
 import com.jjj.permissionutils.R;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity extends AppCompatActivity {
+    private static String[] NEEDED_PERMISSIONS = new String[]{
+            Manifest.permission.CAMERA
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (PermissionUtils.checkPermissions(this, NEEDED_PERMISSIONS)) {
+            PermissionUtils.requestPermissions(this, NEEDED_PERMISSIONS);
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionUtils.onRequestPermissionsResult(this, requestCode, permissions, new PermissionCallback() {
+            @Override
+            public void grantedPermissions() {
+
+            }
+
+            @Override
+            public boolean deniedPermissions() {
+                return false;
+            }
+        });
     }
 }
